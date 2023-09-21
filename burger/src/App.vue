e528cfa315c25aab0275a21bd281c7e3b1db5d0e
Elisabete Rodrigues <elisabete.rodrigues@navarraaluminio.com>
2023-09-21 17:00:15 +0100

    inicio_projeto

--- burger/src/App.vue
+++ burger/src/App.vue
@@ -1,30 +1,31 @@
+<!-- A vue é impressa no componente <router-view/> daí se repetirem -->
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <Navbar :logo="logo_src" :alt="app_name" />
+    <router-view/>
+    <Footer/>
+  </div>
 </template>
 
+<script>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  },
+  data() {
+    return {
+      logo_src: "/img.logo.png",
+      app_name: "burger"
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- burger/src/router/index.js
+++ burger/src/router/index.js
@@ -1,3 +1,7 @@
+// path -> caminho para acessar a rota.
+// name -> qual view vai ser utilizado para acessar a rota.
+// component -> import da rota.
+
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
@@ -8,12 +12,9 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/pedidos',
+    name: 'Pedidos',
+    component: () => import(/* webpackChunkName: "pedidos" */ '../views/Pedidos.vue')
   }
 ]
 
